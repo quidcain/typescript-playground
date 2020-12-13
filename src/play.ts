1a12e90e29746da88917f237b98487e833a67a5f
Daniel Radezky <danielradezky@gmail.com>
2020-12-14 02:18:01 +0300

    Check how any and void work

--- src/play.ts
+++ src/play.ts
@@ -23,3 +23,15 @@ acceptUser({
   lastName: 'Jordan',
   role: 'CEO',
 })
+
+
+const howAnyAndVoidWorks = () => {
+  const identity = (x: any) => x;
+  const foo: (value: string) => void = identity;
+  
+  const four: any = 4;
+  const aNumber: string = four;
+
+  const identity2 = (x: string): number => parseInt(x);
+  const bar: (value: string) => void = identity2;
+};

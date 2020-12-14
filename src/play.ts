f80477c75e522a6be923a008eb39472c8b686303
Daniel Radezky <danielradezky@gmail.com>
2020-12-14 10:22:34 +0300

    Group all play tests

--- src/play.ts
+++ src/play.ts
@@ -1,28 +1,30 @@
-type User = {
-  name: string;
-  lastName: string;
+const structuralTyping = () => {
+  type User = {
+    name: string;
+    lastName: string;
+  }
+
+  function acceptUser<T extends User>(user: T) {
+    console.log(user);
+  }
+
+  const user = {
+    name: 'Oleh',
+    lastName: 'Ivanov',
+  }
+
+  acceptUser(user)
+
+  const dog = { name: 'Bobik' };
+
+  acceptUser(dog);
+
+  acceptUser({
+    name: 'Andrei',
+    lastName: 'Jordan',
+    role: 'CEO',
+  })
 }
-
-function acceptUser<T extends User>(user: T) {
-  console.log(user);
-}
-
-const user = {
-  name: 'Oleh',
-  lastName: 'Ivanov',
-}
-
-acceptUser(user)
-
-const dog = { name: 'Bobik' };
-
-acceptUser(dog);
-
-acceptUser({
-  name: 'Andrei',
-  lastName: 'Jordan',
-  role: 'CEO',
-})
 
 
 const howAnyAndVoidWorks = () => {
